e59618a0c1e46bded4e60907a7ae6cbde76b413a
Anandnand <anandnand5173@gmail.com>
2024-04-09 20:08:07 +0530

    Provided another approach

--- src/LengthOTheLastWord.java
+++ src/LengthOTheLastWord.java
@@ -1,20 +1,39 @@
+import java.util.Scanner;
 
 public class LengthOTheLastWord {
-	public static  int lastWordLength(String s) {
-		int i = s.length() - 1;
+	public static int lastWordLength(String s) {
+//APPROACH 1		
 
-		while (i >= 0 && s.charAt(i) == ' ')
-			--i;
-		final int lastIndex = i;
-		while (i >= 0 && s.charAt(i) != ' ')
-			--i;
+//		int i = s.length() - 1;
+//
+//		while (i >= 0 && s.charAt(i) == ' ')
+//			--i;
+//		final int lastIndex = i;
+//		while (i >= 0 && s.charAt(i) != ' ')
+//			--i;
+//
+//		return lastIndex - i;
 
-		return lastIndex - i;
+//APPROACH 2		
+
+		s = s.trim();
+		int count = 0;
+		for (int i = s.length() - 1; i >= 0; i--) {
+			if (s.charAt(i) != ' ') {
+				count++;
+			} else {
+				break;
+			}
+		}
+		return count;
+
 	}
 
 	public static void main(String[] args) {
-		String s= "   fly me   to   the sunmoon  ";
-		System.out.println(lastWordLength(s) );
+		System.out.println("Enter a String");
+		Scanner sc = new Scanner(System.in);
+		String s = sc.nextLine();
+		System.out.println(lastWordLength(s));
 
 	}
 
